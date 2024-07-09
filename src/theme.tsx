9d1597728a97a2822255908e9768e95cc56b2145
denver0619 <jdrlaurian@gmail.com>
2024-07-09 17:22:10 +0800

    added @mui/styles

---
+++ src/theme.tsx
@@ -0,0 +1,9 @@
+import * as colorData from "../src/assets/material-theme.json";
+import { createContext, useState, useMemo } from "react";
+import {createTheme} from "@mui/material"
+
+const themeTokens = (mode) => {
+    ...{
+        mode === "dark" ?
+    }
+}
