319dcc1a0c558356f86779a275db652e7cbe0ee6
denver0619 <jdrlaurian@gmail.com>
2024-07-09 18:02:01 +0800

    ..

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,9 +1,44 @@
 import * as colorData from "../src/assets/material-theme.json";
 import { createContext, useState, useMemo } from "react";
-import {createTheme} from "@mui/material"
+import { createTheme } from "@mui/material/styles";
 
-const themeTokens = (mode) => {
-    ...{
-        mode === "dark" ?
+export const themeTokens = (mode: String) => ({
+    ...(
+        mode === "dark"
+        ?colorData.schemes.dark
+        :colorData.schemes.light
+    )
+});
+
+export const themeSettings = (mode: String) => {
+    const colors = themeTokens(mode);
+
+    return {
+        pallete: {
+            // mode: mode,
+            // ...(mode === "dark"
+            //     ?colorData.schemes.dark
+            //     :colorData.schemes.light
+            // )
+            ...(colorData.palettes)
+        }
     }
+}
+
+
+export const colorMode = createContext({
+    toggleColorMode: () => {}
+});
+
+export const useMode = () => {
+    const [mode, setMode] = useState("dark");
+
+    const colorMode = useMemo(
+        () => ({
+            toggleColorMode: () => setMode((prev) => (prev === "light" ? "dark" : "light"))
+        }),
+        []
+    )
+
+    const theme = useMemo(() => createTheme(colorData,), [mode])
 }
